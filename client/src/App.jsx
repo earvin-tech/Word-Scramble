00fb7d503031ce586bda37f233cc54a18a995ca0
earvin-tech <wsl@Earvin>
2025-06-02 17:02:18 +1000

    Initialise React, Vite, Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-2xl font-bold text-blue-600">Vite + React + Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
